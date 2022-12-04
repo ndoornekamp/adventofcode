d82faa970aa5e75115a1c08c7df7130112ebbc5a
Nick <ndoornekamp@deloitte.nl>
2022-12-04 02:42:24 -0800

    Ensure date argument is specified

--- scripts/setup-new-day.sh
+++ scripts/setup-new-day.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-folder_name=day$1
+folder_name=day${1:?"Missing argument: please specify the day number (e.g. 01, 02, ...)"}
 mkdir $folder_name -p
 touch $folder_name/input.txt
 touch $folder_name/test_input.txt
