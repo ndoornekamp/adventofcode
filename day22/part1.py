ac2a235b1deb8f1763a84eaf72cc42a269eebf3b
Nick <ndoornekamp@deloitte.nl>
2022-12-22 16:46:28 +0100

    Refactor Day 11 part 1

--- day22/part1.py
+++ day22/part1.py
@@ -30,14 +30,7 @@ for i, row in enumerate(board.split("\n")):
         else:
             raise
 
-location = open_tiles[0]
-direction_idx = 0
-direction = directions[direction_idx]
-
-for instruction in matches:
-    print(instruction)
-    number_of_steps = int(instruction[:-1])
-
+def move(number_of_steps, location, direction):
     for _ in range(number_of_steps):
         next_location = tuple(location + direction)
         if (next_location not in wall_tiles) and (next_location not in open_tiles):
@@ -62,6 +55,18 @@ for instruction in matches:
             location = next_location
         else:
             raise
+    return location
+
+
+location = open_tiles[0]
+direction_idx = 0
+direction = directions[direction_idx]
+
+for instruction in matches:
+    print(instruction)
+    number_of_steps = int(instruction[:-1])
+
+    location = move(number_of_steps, location, direction)
 
     print(f"Direction was {direction}")
     turning_direction = instruction[-1]
@@ -79,30 +84,7 @@ for instruction in matches:
 
 if input[-1] in [str(i) for i in range(10)]:
     number_of_steps = int(input[-1])
-    for _ in range(number_of_steps):
-        next_location = tuple(location + direction)
-        if (next_location not in wall_tiles) and (next_location not in open_tiles):
-            print(f"Next location should be {next_location}, which is not on the board")
-            if direction_idx == 0:  # Moving right -> wrap to left
-                next_location = min([tile for tile in open_tiles + wall_tiles if location[0] == tile[0]], key=lambda x: x[1])
-            elif direction_idx == 2:  # Moving left -> wrap to right
-                next_location = max([tile for tile in open_tiles + wall_tiles if location[0] == tile[0]], key=lambda x: x[1])
-            elif direction_idx == 1:  # Moving down -> wrap up
-                next_location = min([tile for tile in open_tiles + wall_tiles if location[1] == tile[1]], key=lambda x: x[0])
-            elif direction_idx == 3:  # Moving up -> wrap to down
-                next_location = max([tile for tile in open_tiles + wall_tiles if location[1] == tile[1]], key=lambda x: x[0])
-            else:
-                raise
-            print(f"Next location becomes {next_location} instead")
-
-        if next_location in wall_tiles:
-            print(f"Next location {next_location} is a wall tile - stopping and moving on to changing direction")
-            break
-        elif next_location in open_tiles:
-            print(f"Next location {next_location} is an open tile - moving there")
-            location = next_location
-        else:
-            raise
+    location = move(number_of_steps, location, direction)
 
 # 131016 is too low
 print(f"Final row: {location[0] + 1}, final column {location[1] + 1}, final facing {direction_idx}")
