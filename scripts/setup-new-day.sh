e8cf015623b63590cedd6e54f6d525335f72e3e2
Nick Doornekamp <nddoornekamp@gmail.com>
2023-12-02 13:36:13 +0100

    Extend setup script

--- scripts/setup-new-day.sh
+++ scripts/setup-new-day.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-folder_name=day${1:?"Missing argument: please specify the day number (e.g. 01, 02, ...)"}
-mkdir $folder_name -p
-touch $folder_name/input.txt
-touch $folder_name/test_input.txt
-touch $folder_name/part1.py
-touch $folder_name/part2.py
+day="$(date +%d)"
+year="$(date +%Y)"
+folder_name="$year/day$day"
+
+mkdir "$folder_name" -p
+
+touch "$folder_name"/input.txt
+aocd "$year" "$day" > "$folder_name"/input.txt
+
+touch "$folder_name"/test_input.txt
+
+standard_code="
+input_file_path = '$folder_name/test_input.txt'
+
+with open(input_file_path, 'r') as f:
+    input = f.read().splitlines()"
+
+echo "$standard_code" > "$folder_name"/part1.py
+echo "$standard_code" > "$folder_name"/part2.py
