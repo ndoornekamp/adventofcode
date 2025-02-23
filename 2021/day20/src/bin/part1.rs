133b81fccb42f4e59a302a4e65ac9136ba554c2b
Nick Doornekamp <nddoornekamp@gmail.com>
2025-02-23 18:03:18 +0100

    WIP day 20

---
+++ 2021/day20/src/bin/part1.rs
@@ -0,0 +1,47 @@
+fn main() {
+    let input = include_str!("./input.txt");
+
+    let output = solve(input);
+    println!("Part 1 answer: {}", output);
+}
+
+fn solve(input: &str) -> usize {
+    let mut lines = input.lines();
+    let algorithm = lines.next().unwrap();
+
+    lines.next().unwrap();  // empty line
+
+    let mut light_pixels: Vec<(i32, i32)> = vec![];
+    for (row_idx, line) in lines.enumerate() {
+        for (col_idx, char) in line.char_indices() {
+            if char == '#' {
+                light_pixels.push((row_idx as i32, col_idx as i32));
+            }
+        }
+    }
+
+    dbg!(algorithm, light_pixels);
+
+    35
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn example_part_1() {
+        let input = indoc! {"
+            ..#.#..#####.#.#.#.###.##.....###.##.#..###.####..#####..#....#..#..##..###..######.###...####..#..#####..##..#.#####...##.#.#..#.##..#.#......#.###.######.###.####...#.##.##..#..#..#####.....#.#....###..#.##......#.....#..#..#..##..#...##.######.####.####.#.#...#.......#..#.#.#...####.##.#......#..#...##.#.##..#...##.#.##..###.#......#.#.......#.#.#.####.###.##...#.....####.#..#..#.##.#....##..#.####....##...##..#...#......#.#.......#.......##..####..#...#.#.#...##..#.#..###..#####........#..####......#..#
+
+            #..#.
+            #....
+            ##..#
+            ..#..
+            ..###
+        "};
+        let result = solve(input);
+        assert_eq!(result, 35);
+    }
+}
