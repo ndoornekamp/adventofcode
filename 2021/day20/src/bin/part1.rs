e3a4b2b21fc361011561c3981717cfa7fea8b3cd
Nick Doornekamp <nddoornekamp@gmail.com>
2025-05-25 14:31:32 +0200

    Day 20

--- 2021/day20/src/bin/part1.rs
+++ 2021/day20/src/bin/part1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 fn main() {
     let input = include_str!("./input.txt");
 
@@ -9,20 +11,92 @@ fn solve(input: &str) -> usize {
     let mut lines = input.lines();
     let algorithm = lines.next().unwrap();
 
-    lines.next().unwrap();  // empty line
+    lines.next().unwrap(); // empty line
 
-    let mut light_pixels: Vec<(i32, i32)> = vec![];
+    let mut light_pixels: HashSet<(i32, i32)> = HashSet::new();
     for (row_idx, line) in lines.enumerate() {
         for (col_idx, char) in line.char_indices() {
             if char == '#' {
-                light_pixels.push((row_idx as i32, col_idx as i32));
+                light_pixels.insert((row_idx as i32, col_idx as i32));
             }
         }
     }
 
-    dbg!(algorithm, light_pixels);
+    // The infinite background starts as dark, but if the algorithm maps 9 dark pixels to light, that will change.
+    let mut background = '.';
 
-    35
+    for _ in 0..=1 {
+        let (new_pixels, new_background) = apply_algorithm(&algorithm, &light_pixels, background);
+        light_pixels = new_pixels;
+        background = new_background;
+    }
+
+    light_pixels.len()
+}
+
+fn apply_algorithm(
+    algorithm: &str,
+    light_pixels: &HashSet<(i32, i32)>,
+    background: char,
+) -> (HashSet<(i32, i32)>, char) {
+    let min_row = light_pixels.iter().map(|&(r, _)| r).min().unwrap();
+    let max_row = light_pixels.iter().map(|&(r, _)| r).max().unwrap();
+    let min_col = light_pixels.iter().map(|&(_, c)| c).min().unwrap();
+    let max_col = light_pixels.iter().map(|&(_, c)| c).max().unwrap();
+
+    let mut new_light_pixels = HashSet::new();
+    for row in (min_row - 1)..=(max_row + 1) {
+        for col in (min_col - 1)..=(max_col + 1) {
+            let mut index = 0;
+            let mut binary_string = String::new();
+            for dr in -1..=1 {
+                for dc in -1..=1 {
+                    index <<= 1;
+                    let pos = (row + dr, col + dc);
+                    let bit = if row + dr < min_row
+                        || row + dr > max_row
+                        || col + dc < min_col
+                        || col + dc > max_col
+                    {
+                        background
+                    } else if light_pixels.contains(&pos) {
+                        '#'
+                    } else {
+                        '.'
+                    };
+
+                    if bit == '#' {
+                        binary_string.push('1');
+                    } else {
+                        binary_string.push('0');
+                    }
+                }
+            }
+            index = usize::from_str_radix(&binary_string, 2).unwrap();
+
+            if algorithm.chars().nth(index).unwrap() == '#' {
+                new_light_pixels.insert((row, col));
+            }
+        }
+    }
+
+    // Update background: if it was dark, it becomes light if the algorithm maps 9 dark pixels to light; dark otherwise
+    let new_background: char = if background == '.' {
+        if algorithm.chars().nth(0).unwrap() == '#' {
+            '#'
+        } else {
+            '.'
+        }
+    } else {
+        // If the background was light, it remains light if the algorithm maps 9 light pixels to light; dark otherwise
+        if algorithm.chars().last().unwrap() == '#' {
+            '#'
+        } else {
+            '.'
+        }
+    };
+
+    (new_light_pixels, new_background)
 }
 
 #[cfg(test)]
